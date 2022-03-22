5e99e3a34d746a4f89a5976d3f8ecbd4077e8d28
zhangjiayi1526183832 <1526183832@qq.com>
2022-03-22 16:08:41 +0800

    第二次传入

--- Demo06/Demo06.cpp
+++ Demo06/Demo06.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 struct SNode {
-    int data;
+    char data;
     SNode* next;
 };
 typedef SNode* Stack;
@@ -13,17 +13,17 @@ Stack CreateStack()
     //创建一个新的头结点
     Stack s;
     s = new SNode();
-    s->next == nullptr;
+    s->next = nullptr;
     return s;
 }
 
-int IsEmpty(Stack s)
+bool IsEmpty(Stack s)
 {
     //判断堆栈是否为空
     return (s->next == nullptr);
 }
 
-void Push(int x, Stack s)
+void Push(char x, Stack s)
 {
     Stack newnode = new SNode();
     newnode->data = x;
@@ -31,25 +31,85 @@ void Push(int x, Stack s)
     s->next = newnode;
 }
 
-int Pop(Stack s)
+char Pop(Stack s)
 {
     if (IsEmpty(s))
     {
         cout << "堆栈为空" << endl;
-        return -1;
+        return '\0';
     }
     else
     {
         Stack firstcell = s->next;
-        int x = firstcell->data;
+        char x = firstcell->data;
         s->next = firstcell->next;
         delete firstcell;
         return x;
     }
 }
 
+//括号匹配问题
+bool Match(char a, char b)
+{
+    //判断两个括号是否匹配
+    if (a == '(' && b == ')')
+        return true;
+    if (a == '[' && b == ']')
+        return true;
+    if (a == '{' && b == '}')
+        return true;
+    return false;
+}
+
+void BracketMatch(char* str)
+{
+    Stack s = CreateStack();
+    for (size_t i = 0; str[i]!='\0'; i++)
+    {
+        switch (str[i])
+        {
+        case '(':
+        case '[':
+        case '{':
+            Push(str[i], s);
+            break;
+        case ')':
+        case ']':
+        case '}':
+            if (IsEmpty(s))
+            {
+                cout << "不匹配" << endl;
+                return;
+            }
+            else
+            {
+                if (Match(Pop(s), str[i]))
+                {
+                    continue;
+                }
+                else
+                {
+                    cout << "不匹配" << endl;
+                    return;
+                }
+            }
+        }
+        if (IsEmpty(s))
+        {
+            cout << "匹配" << endl;
+            return;
+        } 
+        else
+        {
+            cout << "不匹配" << endl;
+            return;
+        }
+            
+    }
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    char c[] = "()[]";
+    BracketMatch(c);
 }
 
