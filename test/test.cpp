0f84b234fcdf4839d68c194a0e7d2b0f21e670c5
zhangjiayi1526183832 <1526183832@qq.com>
2022-04-12 08:21:06 +0800

    提交

---
+++ test/test.cpp
@@ -0,0 +1,67 @@
+﻿// 约瑟夫环问题
+#include <iostream>
+using namespace std;
+
+struct LNode
+{
+    int data;
+    LNode* next;
+};
+
+typedef LNode* LinkedList;
+// 尾插法创建循环单链表
+//此时n表示在环内的人数
+LinkedList create(int n)
+{
+    LNode* L = new LNode();//L表示头结点
+    L->data = 1;
+    L->next = L; // 只有一个节点时指向自身成环
+    for (int i = 2; i <= n; i++)
+    {
+        LNode* s = new LNode();//表示插入的元素节点
+        s->data = i;
+        s->next = L->next;
+        L->next = s;
+        L = s;//此时s成为新的头结点
+    }
+    return L;
+}
+
+// 删除节点，此时m表示练到m就出列
+void nodeDelete(LinkedList& L, int m)
+{
+    LNode* front = L;//指向报数的前一个元素
+    LNode* p = L->next;//指向被删除的元素
+    int count = 1;//计数器
+    while (p != front)//判断此时场上是否只有一个人
+    {
+        if (count < m)
+        {
+            p = p->next;
+            front = front->next;
+            count++;//没报数到m，数据正常后移
+        }
+        else
+        {
+            cout << p->data << " ";//打印出列的序号
+            LNode* s=p;
+            front->next = p->next;
+            p = p->next;
+            count = 1;//计数器重置
+            delete s;//删除多余空间
+        }
+    }
+    cout << p->data << endl;//打印最后一个人的序号
+    delete p;//删除空间
+}
+
+
+int main()
+{
+    int m, n;
+    cout << "请输入m与n" << endl;
+    cin >> m >> n;
+    LinkedList L = create(n);
+    nodeDelete(L, m);
+}
+
---
+++ test1/test1.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+using namespace std;
+struct Node
+{
+    int coef;//系数
+    int exp;//指数
+    Node* next;
+};
+typedef Node* Polynomial;
+
+void insertPoly(int c, int e, Polynomial *rear)
+{
+    Polynomial p = new Node();
+    p->coef = c;
+    p->exp = e;
+    p->next = nullptr;
+    (*rear)->next = p;
+    *rear = p;
+}
+
+Polynomial readPoly()
+{
+    Polynomial p,rear,t;
+    p = new Node();
+    p->next = nullptr;
+    rear = p;
+    cout << "请输入多项式项数" << endl;
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int c;
+        int e;
+        cin >> c >> e;
+        insertPoly(c, e, &rear);
+    }
+    t = p;
+    p = p->next;
+    delete t;
+    return p;
+}
+
+Polynomial addPoly(Polynomial p1, Polynomial p2)
+{
+    Polynomial sum = new Node();
+    Polynomial t1 = p1;
+    Polynomial t2 = p2;
+    Polynomial rear = sum;
+    while (t1 && t2)
+    {
+        if (t1->exp == t2->exp)
+        {
+            if ((t1->coef + t2->coef) == 0)
+            {
+                t1 = t1->next;
+                t2 = t2->next;
+                continue;
+            }
+            insertPoly(t1->coef + t2->coef, t1->exp, &rear);
+            t1 = t1->next;
+            t2 = t2->next;
+        }
+        else if (t1->exp > t2->exp)
+        {
+            insertPoly(t1->coef, t1->exp, &rear);
+            t1 = t1->next;
+        }
+        else
+        {
+            insertPoly(t2->coef, t2->exp, &rear);
+            t2 = t2->next;
+        }
+    }
+    while (t1)
+    {
+        insertPoly(t1->coef, t1->exp, &rear);
+        t1 = t1->next;
+    }
+    while (t2)
+    {
+        insertPoly(t2->coef, t2->exp, &rear);
+        t2 = t2->next;
+    }
+    Polynomial t = sum;
+    sum = sum->next;
+    delete t;
+    return sum;
+}
+
+void printPoly(Polynomial p)
+{
+    while (p)
+    {
+        cout << p->coef <<" " << p->exp << endl;
+        p = p->next;
+    }
+}
+
+int main()
+{
+    Polynomial p1, p2, sum;
+    cout << "请输入第一个多项式" << endl;
+    p1 = readPoly();
+    cout << "请输入第二个多项式" << endl;
+    p2 = readPoly();
+    sum = addPoly(p1, p2);
+    cout << "相加之后的多项式为" << endl;
+    printPoly(sum);
+}
+
